91366c71561ebd1d3c351a955745ecfb9b41698e
ziad <ziad11nasr11@gmail.com>
2026-06-09 09:54:26 +0300

    add email sharing feature for blog posts

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .modles import Post
 from django.core.paginator import Paginator
-
+from django.core.mail import send_mail
+from .forms import EMAILPOSTFORM
 def post_list(request):
     posts = Post.PUBLISHED.all().order_by('-id')
     paginator = Paginator(post_list,10)
@@ -20,3 +21,15 @@ def post_detail(request, year, month, day, slug):
 
     #return render(  request,"blog/post_detail.html",{"post": post}, )
 
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+
+    #return render(request,"blog/post/share.html",{"post": post,"form": form},)
