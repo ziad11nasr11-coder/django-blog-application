cd496e6ed7abcebc45815071ab2eaff15caaac7c
ziad <ziad11nasr11@gmail.com>
2026-06-10 06:26:36 +0300

    enhance post share view

--- src/posts/views.py
+++ src/posts/views.py
@@ -24,12 +24,47 @@ def post_detail(request, year, month, day, slug):
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id)
 
+    sent = False
+
     if request.method == "POST":
         form = EmailPostForm(request.POST)
 
         if form.is_valid():
             cd = form.cleaned_data
+
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+
+            subject = (
+                f"{cd['name']} recommends you read "
+                f"{post.title}"
+            )
+
+            message = (
+                f"Read '{post.title}' at {post_url}\n\n"
+                f"{cd['name']}'s comments:\n"
+                f"{cd['comments']}"
+            )
+
+            send_mail(
+                subject,
+                message,
+                None,
+                [cd["to"]]
+            )
+
+            sent = True
+
     else:
         form = EmailPostForm()
 
-    #return render(request,"blog/post/share.html",{"post": post,"form": form},)
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post": post,
+            "form": form,
+            "sent": sent,
+        },
+    )
