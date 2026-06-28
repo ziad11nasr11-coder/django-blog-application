939fc0cf0d2f422f817a37c39f251275fbc6e43a
ziad nasr-eldin <ziad11nasr11@gmail.com>
2026-06-28 17:20:23 +0300

    fix bug in post detail view

--- src/posts/views.py
+++ src/posts/views.py
@@ -51,7 +51,8 @@ def post_detail(request, year, month, day, slug):
 
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = (
-        Post.published.filter(tags__in=post_tags_ids)
+        Post.objects.published()
+        .filter(tags__in=post_tags_ids)
         .exclude(id=post.id)
         .annotate(same_tags=Count('tags'))
         .order_by('-same_tags', '-published_at')[:4]
