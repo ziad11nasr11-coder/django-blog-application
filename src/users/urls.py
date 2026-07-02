59b11e4ddc3e529b50e40db2b115045900adf04f
ziad <ziad11nasr11@gmail.com>
2026-07-02 07:25:35 +0300

    Implement login functionality using Django auth (view, form, urls)

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import include
+from django.urls import path
+from .views import login_view
 
 urlpatterns = [
-    
+    path("login/", login_view, name="login"),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import LoginForm
 
-# Create your views here.
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("index")
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("index")
+            else:
+                form.add_error(None, "Invalid username or password")
+
+    return render(request, "userss/login.html", {"form": form})
