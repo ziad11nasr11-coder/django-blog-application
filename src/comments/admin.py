e893f69b0c2ee0bcc11ebf5538bf61dd0d0356eb
ziad nasr-eldin <ziad11nasr11@gmail.com>
2026-07-20 06:50:03 +0300

    register Comment model in admin

--- src/comments/admin.py
+++ src/comments/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "post",
+        "status",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "created_at",
+    )
+    search_fields = (
+        "body",
+        "author__username",
+        "post__title",
+    )
+    ordering = ("-created_at",)
+    autocomplete_fields = (
+        "author",
+        "post",
+    )
