15bb16767df73103bf7426b6469fad1e0f02bf46
ziad nasr-eldin <ziad11nasr11@gmail.com>
2026-07-20 06:52:00 +0300

    add CommentForm

---
+++ src/comments/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("body",)
+        widgets = {
+            "body": forms.Textarea(
+                attrs={
+                    "rows": 4,
+                    "placeholder": "Write your comment...",
+                }
+            ),
+        }
